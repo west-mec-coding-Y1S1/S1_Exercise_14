d6b071c316cce094c53c53514e3029b83130ffc0
Patrick Clawson <patrick.clawson@west-mec.org>
2019-04-01 15:23:22 -0700

    Stuff

--- bc_outline.js
+++ bc_outline.js
@@ -54,12 +54,26 @@ function createList(source, outlineList) {
    var headings = ["H1", "H2", "H3", "H4", "H5", "H6"];
    //previous level of the headings
    var prevLevel = 0;
+   //running total of the article headings
+   var headNum = 0;
    //loop thorugh all of the child nodes of source article until no child nodes are left
    for (var n = source.firstChild; n !== null; n = n.nextSibling) {
       var headLevel = headings.indexOf(n.nodeName);
       if (headLevel !== -1) {
+         //add an id to the heading if it is missing
+         headNum++;
+         if (n.hasAttribute("id") === false) {
+            n.setAttribute("id", "head" + headNum);
+         }
          var listElem = document.createElement("li");
-         listElem.innerHTML = n.firstChild.nodeValue;
+         //create hypertext links to the document headings
+         var linkElem = document.createElement("a");
+         linkElem.innerHTML = n.innerHTML;
+         linkElem.setAttribute("href", "#" + n.id);
+
+         //append the hypertext link to the list item
+         listElem.appendChild(linkElem);
+
          if (headLevel === prevLevel) {
             //append the list item to the current list
             outlineList.appendChild(listElem);
@@ -73,6 +87,12 @@ function createList(source, outlineList) {
             outlineList = nestedList;
          } else {
             //append the list item to a higher list
+            var levelUp = prevLevel - headLevel;
+            //go up to the higher level
+            for (var i = 1; i <= levelUp; i++) {
+               outlineList = outlineList.parentNode.parentNode;
+            }
+            outlineList.appendChild(listElem);
          }
 
          //update the value of prevLevel
